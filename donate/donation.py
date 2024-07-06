0540ac285f39289bf36ded1e6c689de5f2caf63a
´lrondan´ <lrondan1126@gmail.com>
2024-07-06 11:24:46 +0200

    donate complete

--- donate/donation.py
+++ donate/donation.py
@@ -1,14 +1,4 @@
 from tkinter import *
-from tkinter import PhotoImage
+from tkinter import messagebox as mBox
 
-class Root:
-    def __init__(self, interfaz) -> None:
-        self.gui = interfaz
-        self.gui.resizable(0,0)
-        self.gui.title('Doname algo')
-
-        self.imagen = PhotoImage(file='donate/BTCQR.png')
-        Label(self.gui, image=self.imagen).pack()
-
-obj=Root(Tk())
-obj.gui.mainloop()
+mBox.showinfo('DONAME','BTC:\n 12Uj5zjsoCqCBgS1DHAi8w4u4okuVTCpuN')
